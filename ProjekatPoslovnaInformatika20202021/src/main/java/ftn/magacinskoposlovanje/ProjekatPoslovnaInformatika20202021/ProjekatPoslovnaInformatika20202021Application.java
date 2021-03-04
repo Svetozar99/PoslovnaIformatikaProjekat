74238d375cbb895e90de641d9a974266f66c3485
Svetozar99 <svetozarbrboric@gmail.com>
2021-03-04 16:11:41 +0100

    Inicijalni komit

---
+++ ProjekatPoslovnaInformatika20202021/src/main/java/ftn/magacinskoposlovanje/ProjekatPoslovnaInformatika20202021/ProjekatPoslovnaInformatika20202021Application.java
@@ -0,0 +1,13 @@
+package ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatPoslovnaInformatika20202021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatPoslovnaInformatika20202021Application.class, args);
+	}
+
+}
---
+++ ProjekatPoslovnaInformatika20202021/src/test/java/ftn/magacinskoposlovanje/ProjekatPoslovnaInformatika20202021/ProjekatPoslovnaInformatika20202021ApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatPoslovnaInformatika20202021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
