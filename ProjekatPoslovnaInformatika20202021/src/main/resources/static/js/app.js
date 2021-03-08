3e944f8adcca9f287f6a0d285a3400fe574f55c8
Svetozar99 <svetozarbrboric@gmail.com>
2021-03-08 10:39:57 +0100

    popravljen prikaz i dodavanje preduzeca

--- ProjekatPoslovnaInformatika20202021/src/main/resources/static/js/app.js
+++ ProjekatPoslovnaInformatika20202021/src/main/resources/static/js/app.js
@@ -3,6 +3,8 @@ var prikaziPrijemnicu = false;
 var prikaziMedjumagacinskiPromet = false;
 var counter = 0;
 var redovi = [];
+var prikaziPreduzeca = false;
+var dodavanjePreduzeca = false;
 
 function odrediPrikaz(nazivDokumenta){
     if(nazivDokumenta === "otpremnica"){
@@ -17,6 +19,17 @@ function odrediPrikaz(nazivDokumenta){
         prikaziMedjumagacinskiPromet = true;
         prikaziOtpremnicu = false;
         prikaziPrijemnicu = false;
+    }else if(nazivDokumenta === "svaPreduzeca"){
+        prikaziPreduzeca = true;
+        prikaziOtpremnicu = false;
+        prikaziPrijemnicu = false;
+        prikaziMedjumagacinskiPromet = false;
+    }else if(nazivDokumenta === "dodajPreduzece"){
+        dodajPreduzece = true;
+        prikaziPreduzeca = false;
+        prikaziOtpremnicu = false;
+        prikaziPrijemnicu = false;
+        prikaziMedjumagacinskiPromet = false;
     }
     prikazi();
 }
@@ -36,10 +49,17 @@ function prikazi(){
     medjumagacinskiPromet.hide()
     if(prikaziPrijemnicu){
         prijemnica.show();
+        $("#preduzecaTable").hide();
     }else if(prikaziOtpremnicu){
         otpremnica.show();
     }else if(prikaziMedjumagacinskiPromet){
         medjumagacinskiPromet.show();
+    }else if(prikaziPreduzeca){
+        $("#prometniDokment").hide();
+    }else if(dodajPreduzece){
+        $("#preduzecaTable").hide();
+        $("#prometniDokment").hide();
+        $("#dodajPreduzece").show();
     }
 }
 
@@ -88,14 +108,89 @@ function proknjizi(){
 function promeniIzgledTaba(dropdown){
     var prometniDokumentDropdown = $("#prometniDokumentDropdown");
     var robeUslugeDropdown = $("#robeUslugeDropdown");
+    var preduzeceDropdown = $("preduzeceDropdown");
     if(dropdown === "prometniDokumentDropdown"){
         console.log("prometniDokumentDropdown")
         prometniDokumentDropdown.addClass("active");
+        preduzeceDropdown.removeClass("active");
         robeUslugeDropdown.removeClass("active");
     }
     else if(dropdown === "robeUslugeDropdown"){
         console.log("robeUslugeDropdown")
         robeUslugeDropdown.addClass("active");
+        preduzeceDropdown.removeClass("active");
         prometniDokumentDropdown.removeClass("active");
     }
+    else if(dropdown === "preduzeceDropdown"){
+        console.log("preduzeceDropdown")
+        preduzeceDropdown.addClass("active");
+        robeUslugeDropdown.removeClass("active");
+        prometniDokumentDropdown.removeClass("active");
+    }
+}
+function prikazSvihPreduzeca() {
+
+    var tabelaPreduzeca = $("#preduzecaTable");
+    odrediPrikaz('svaPreduzeca');
+    function prikaziPreduzeca(){
+        $.ajax({
+            type: "GET",
+            contentType : 'application/json; charset=utf-8',
+            url : "http://localhost:8080/api/preduzece",
+            success : function(result){
+                tabelaPreduzeca.show();
+                tabelaPreduzeca.empty();
+                for(preduzece in result){
+                    tabelaPreduzeca.append(
+                        '<tr>'
+							+'<td align="center">'+'<a href="" id="prikaziJedno" result-prID="'+result[preduzece].id+'">'+result[preduzece].naziv+'</a>'+'</td>'
+							+'<td align="center">'+result[preduzece].adresa+'</td>'
+							+'<td align="center">'+result[preduzece].telefon+'</td>'
+							+'<td align="center">'+result[preduzece].pIB+'</td>'
+							+'<td align="center">'+result[preduzece].mIB+'</td>'
+							+'<td/>'
+						+'</tr>'
+                    )};
+                    selectedId = $(this).attr('result-prID');
+                    
+            },
+            error :function(e){
+                alert('ne valja nesto');
+            }
+        });
+    }
+    prikaziPreduzeca();
+
+    console.log('aaaaa');
+}
+
+function submitPreduzece(){
+
+    var nazivInput = $("#naziv");
+    var adresaInput = $("#adresa");
+    var brojTelefonaInput = $("#brojTelefona");
+    var pibInput = $("#pib");
+    var mibInput = $("#mib");
+
+    var formData = {
+        "naziv": nazivInput.val(),
+        "adresa": adresaInput.val(),
+        "telefon": brojTelefonaInput.val(),
+        "pIB": pibInput.val(),
+        "mIB": mibInput.val()
+    }
+
+    $.ajax({
+        url : 'http://localhost:8080/api/preduzece',
+        type : "POST",
+        contentType: 'application/json; charset=utf-8',
+        data : JSON.stringify(formData),
+        success: function(result){
+            alert('Preduzece uspjesno dodato');
+        },
+        error : function(e){
+            alert('Doslo je do neke greške!')
+            console.log("ERROR: ", e);
+        }
+    });
 }
