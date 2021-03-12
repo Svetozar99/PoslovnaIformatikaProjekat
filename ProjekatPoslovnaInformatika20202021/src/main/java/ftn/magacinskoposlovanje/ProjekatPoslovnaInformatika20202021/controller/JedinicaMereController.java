18bdccbbbc3c4815917513641506cb99ab5774db
milankrunic <milankrunic95@gmail.com>
2021-03-12 16:47:05 +0100

    Uradjeno prikaz i dodavanje jedinica mera

--- ProjekatPoslovnaInformatika20202021/src/main/java/ftn/magacinskoposlovanje/ProjekatPoslovnaInformatika20202021/controller/JedinicaMereController.java
+++ ProjekatPoslovnaInformatika20202021/src/main/java/ftn/magacinskoposlovanje/ProjekatPoslovnaInformatika20202021/controller/JedinicaMereController.java
@@ -7,12 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.entityDTO.JedinicaMereDTO;
+import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.entityDTO.MagacinDTO;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.entityDTO.PreduzeceDTO;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.model.JedinicaMere;
+import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.model.Magacin;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.model.Preduzece;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.serviceInterface.JedinicaMereServiceInterface;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.serviceInterface.PreduzeceServiceInterface;
@@ -33,5 +38,16 @@ public class JedinicaMereController {
 			dtos.add(new JedinicaMereDTO(j));
 		}
 		return new ResponseEntity<List<JedinicaMereDTO>>(dtos, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<JedinicaMereDTO> addMagacin(@RequestBody JedinicaMereDTO jedinicaMereDTO){
+
+		JedinicaMere jm = new JedinicaMere();
+		jm.setNaziv(jedinicaMereDTO.getNaziv());
+		jm.setSkraceniNaziv(jedinicaMereDTO.getSkraceniNaziv());
+		
+		jm = jedinicaMereServiceInterface.save(jm);
+		return new ResponseEntity<JedinicaMereDTO>(new JedinicaMereDTO(jm), HttpStatus.CREATED);
 	}
 }
