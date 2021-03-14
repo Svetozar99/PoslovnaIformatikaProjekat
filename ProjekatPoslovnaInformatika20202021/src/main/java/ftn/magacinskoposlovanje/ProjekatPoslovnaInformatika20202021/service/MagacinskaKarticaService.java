b9c2b8b350695bdd6e064539df3ef000d531d0b3
Svetozar99 <svetozarbrboric@gmail.com>
2021-03-14 22:19:19 +0100

    Ako magacinska kartica ne postoji automacki se napravi - backend

--- ProjekatPoslovnaInformatika20202021/src/main/java/ftn/magacinskoposlovanje/ProjekatPoslovnaInformatika20202021/service/MagacinskaKarticaService.java
+++ ProjekatPoslovnaInformatika20202021/src/main/java/ftn/magacinskoposlovanje/ProjekatPoslovnaInformatika20202021/service/MagacinskaKarticaService.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.model.Magacin;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.model.MagacinskaKartica;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.model.PoslovnaGodina;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.model.RobaIliUsluga;
+import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.repository.MagacinRepository;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.repository.MagacinskaKarticaRepository;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.repository.PoslovnaGodinaRepository;
 import ftn.magacinskoposlovanje.ProjekatPoslovnaInformatika20202021.repository.RobaIliUslugaRepository;
@@ -25,6 +27,9 @@ public class MagacinskaKarticaService implements MagacinskaKarticaServiceInterfa
 	
 	@Autowired
 	PoslovnaGodinaRepository poslovnaGodinaRepository;
+	
+	@Autowired
+	MagacinRepository magacinRepository;
 	
 	@Override
 	public List<MagacinskaKartica> findAll() {
@@ -46,10 +51,58 @@ public class MagacinskaKarticaService implements MagacinskaKarticaServiceInterfa
 			Integer poslovnaGodinaId){
 		RobaIliUsluga robaIliUsluga = robaIliUslugaRepository.findOneBySifra(robaIliUslugaId);
 		PoslovnaGodina poslovnaGodina = poslovnaGodinaRepository.findOneByIdGodine(poslovnaGodinaId);
+
+		Magacin magacin = magacinRepository.findOneBySifraMagacina(1);
+		
+		List<MagacinskaKartica> getAllMagKart = magacinskaKarticaRepository.findAll();
 		
 		List<MagacinskaKartica> magacinskeKarticeRiliL = robaIliUsluga.getMagacinskeKartice();
 		List<MagacinskaKartica> magacinskeKarticePg = poslovnaGodina.getMagacinskeKartice();
 
+		int i = getAllMagKart.size();
+		System.out.println(i + "broj magacinskih kartica u bazi");
+		
+		if(magacinskeKarticeRiliL.isEmpty()) {
+			System.out.println("za ovu robu ili uslugu ne postoji ni jedna magacinska kartica");
+			MagacinskaKartica m = new MagacinskaKartica();
+//			m.setId(i+1);
+			m.setPocetnoStanjeKolicinski(0);
+			m.setPrometUlazaKolicinski(0);
+			m.setPrometIzlazaKolicinski(0);
+			m.setUkupnaKolicina(0);
+			m.setPocetnoStanjeVrednosno(0);
+			m.setPrometIzlazaKolicinski(0);
+			m.setPrometUlazaVrednosno(0);
+			m.setUkupnaVrednost(0);
+			m.setCena(120);
+			m.setMagacin(magacin);
+			m.setRobaIliUsluga(robaIliUsluga);
+			m.setPoslovnaGodina(poslovnaGodina);
+			
+			magacinskeKarticeRiliL.add(m);
+			magacinskaKarticaRepository.save(m);
+		}
+		if(magacinskeKarticePg.isEmpty()) {
+			System.out.println("za ovu poslovnu godinu ne postoji nijedna magacinska kartica");
+			MagacinskaKartica m = new MagacinskaKartica();
+//			m.setId(i+1);
+			m.setPocetnoStanjeKolicinski(0);
+			m.setPrometUlazaKolicinski(0);
+			m.setPrometIzlazaKolicinski(0);
+			m.setUkupnaKolicina(0);
+			m.setPocetnoStanjeVrednosno(0);
+			m.setPrometIzlazaKolicinski(0);
+			m.setPrometUlazaVrednosno(0);
+			m.setUkupnaVrednost(0);
+			m.setCena(120);
+			m.setMagacin(magacin);
+			m.setRobaIliUsluga(robaIliUsluga);
+			m.setPoslovnaGodina(poslovnaGodina);
+			
+			magacinskeKarticePg.add(m);
+			magacinskaKarticaRepository.save(m);
+		}
+		
 		for(MagacinskaKartica mkpg: magacinskeKarticePg) {
 			for(MagacinskaKartica mkru: magacinskeKarticeRiliL) {
 				if(mkpg.getId() == mkru.getId()) {
